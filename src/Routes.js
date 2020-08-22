3f5a27d1b509da3fca0c224e9249d359a23d5da1
davidcwh <dcwh12321@gmail.com>
2020-08-23 00:01:26 +0800

    Add 404 Not Found page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LandingPage from "./containers/LandingPage";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <LandingPage />
+      </Route>
+
+
+      {/* Finally, catch all unmatched routes */}
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   );
---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/NotFound.css";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1>Sorry, page not found!</h1>
+    </div>
+  );
+}
